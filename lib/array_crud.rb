80990215bf729565982f67b8f99e52d859057ff2
Dennis Lord <lorddennis1006@gmail.com>
2018-05-02 21:12:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,8 +36,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array = ["array"]
-    retrieve_first_element_from_array[1]
+  retrieve_first_element_from_array = ["wow","array"]
+    retrieve_first_element_from_array[0]
 
 end
 
